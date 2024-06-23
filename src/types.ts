faba598aec1e7f11c48eb1aa6c67178bf9d374f4
Jun Xiang (カエデ) <contact@ngjx.org>
2024-06-23 16:48:12 +0800

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+// Place some shared types here
+
+export interface ProjectInfo {
+  name: string;
+  email: string;
+  username: string;
+  repository: string;
+  proj_name: string;
+  proj_short_desc: string;
+  proj_long_desc: string;
+  docs_url: string;
+  assignees: string;
+}
